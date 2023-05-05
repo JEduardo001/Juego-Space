8e0f251aabc5ef06e8f01b9c5b8b0a9e0a5e0c2f
JEduardo001 <125694974+JEduardo001@users.noreply.github.com>
2023-05-05 00:45:26 -0600

    Update Enemigo.java

--- Space/src/Space/Enemigo.java
+++ Space/src/Space/Enemigo.java
@@ -5,16 +5,17 @@ import java.awt.Color;
 public class Enemigo {
 
 
-	int x,y,largo,alto;
+	int x,y,largo,alto, vida;
 	Color color;
 	int direccion;
 	
-	public Enemigo(int x,int y,int largo,int alto,Color color,int direccion) {
+	public Enemigo(int x,int y,int largo,int alto,Color color,int direccion,int vida) {
 		this.x=x;
 		this.y=y;
 		this.largo=largo;
 		this.alto=alto;
 		this.color=color;
 		this.direccion=direccion;
+		this.vida=vida;
 	}
 }
