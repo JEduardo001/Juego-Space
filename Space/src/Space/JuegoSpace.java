f27afb0be5ba94d44726ab3cfc95dce3fc6b3838
JEduardo001 <125694974+JEduardo001@users.noreply.github.com>
2023-05-05 23:47:50 -0600

    Update JuegoSpace.java

--- Space/src/Space/JuegoSpace.java
+++ Space/src/Space/JuegoSpace.java
@@ -37,25 +37,31 @@ public class JuegoSpace {
 	JPanel tablero = new JPanel();
 	
 	//jugador
-	static int jugadorX=200,jugadorY=150,speedJugador=40,vidaJugador=1000;
-	static int cordJugador[][]= new int[4][4]; 
-	private Image enemigo1,enemigo2,enemigo3,boss;
-	private MediaTracker tracker;
-	private int frameIndex = 0;
-	private boolean isRunning = false;
+	static int jugadorX=200,jugadorY=380,speedJugador=20,vidaJugador=1000;
+	static int cordJugador[][]= new int[2][4]; 
 	
-
-	static JLabel lblNewLabelVidaJugador = new JLabel("V I D A  ||   "+vidaJugador);
-	static int cordProyectilesJugador[][]= new int[50][3];
+	//static JLabel jLabelVidaJugador = new JLabel("V I D A  ||   "+vidaJugador);
+	//static int cordProyectilesJugador[][]= new int[50][3];
+	static int balaJugadorX,balaJugadorY;
 	//juego
 	static boolean gameOver=false;
 	static int limiteX=470;
 	static int limiteY=410;
 	static Random rand = new Random();
+	
 	//mapa
 	Mapa cordBordesMapa[]= new Mapa[4];
 	//Enemigo
-	Enemigo enemigos[] =new Enemigo[68];
+	public static Enemigo enemigos[] =new Enemigo[68];
+	public static int balaEnemigaX;
+	public static int balaEnemigaY;
+	//public static int proyectilEnemigo[][]=new int[5][2];
+	public static int vidaBoss=5;
+
+	private Image enemigo1,enemigo2,enemigo3,boss;
+	private MediaTracker tracker;
+	private int frameIndex = 0;
+	private boolean isRunning = false;
 
 	/**
 	 * Launch the application.
@@ -78,10 +84,6 @@ public class JuegoSpace {
 	 */
 	public JuegoSpace() {
 		initialize();
-	
-	
-	
-	
 		
 	}
 	
@@ -123,7 +125,7 @@ public class JuegoSpace {
 	TimerTask moverEnemigos = new TimerTask() {
         public void run() {
          
-        //	moverEnemigos();
+        	moverEnemigos();
         }
 	};
 	
@@ -134,77 +136,123 @@ public class JuegoSpace {
         	//cambiarDireccionEnemiga();
         }
 	};
+	TimerTask genProyectilEnemigo = new TimerTask() {
+        public void run() {
+         
+        	genProyectilEnemigo();
+        }
+	};
+	TimerTask moverProyectilEnemigo = new TimerTask() {
+        public void run() {
+         
+        	moverProyectilEnemigo();
+        }
+	};
+	TimerTask moverBoss = new TimerTask() {
+        public void run() {
+         
+        	moverBoss();
+        }
+	};
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	public static void genProyectilJugador() {
-		for(int i=0;i<50;i++) {
-			
-				if(cordProyectilesJugador[i][0]<=0 || cordProyectilesJugador[i][0]>=limiteX || 
-					cordProyectilesJugador[i][1]>=limiteY || cordProyectilesJugador[i][1]<=0  ) {
+	
+				if(balaJugadorX==0) {
 					
-					cordProyectilesJugador[i][0]=cordJugador[1][0];
-					cordProyectilesJugador[i][1]=cordJugador[1][1];
-					cordProyectilesJugador[i][2]=rand.nextInt(4);
-					break;	
+					balaJugadorX=cordJugador[1][0];
+					balaJugadorY=cordJugador[1][1];
 				}
-		}
+		
 	}
 	
-	
+		//esto es para comprobar si la bala del jugador choco contra el enemigo
+	public static void colicion() {
+		
+			
+			for(int i2=0;i2<enemigos.length;i2++) {
+				
+				if(balaJugadorX<=enemigos[i2].x+30 && balaJugadorX>=enemigos[i2].x
+						&& balaJugadorY>enemigos[i2].y && balaJugadorY<enemigos[i2].y+30) {
+				
+					System.out.println("impacto bala jugador con enemigo linea: 180");
+
+					if(i2==0) {
+						System.out.println("impacto con boss");
+
+						if(vidaBoss-1==0) {
+							System.out.println("BOSS MUERTOOOOOO");
+						}else {
+							vidaBoss--;
+
+						}
+						
+						
+					}else {
+						balaJugadorX=0;
+						balaJugadorY=0;
+						enemigos[i2].x=0;
+						enemigos[i2].y=0;	
+					}
+					
+					
+					
+				}
+			}
+		
+	}
 	public static void moverProyectilJugador() {
-  	  
-		 for(int i=0;i<cordProyectilesJugador.length;i++) {
-         	//-1 es una cordenada fuera del tablero lo que va a indicar que no hay nada ahi 
-         
-         		if(cordProyectilesJugador[i][1]-20>0) {
+  
+         		if(balaJugadorY-20>0) {
          			
-         			cordProyectilesJugador[i][1]-=20;
+         			balaJugadorY-=20;
          		}else {
-         			cordProyectilesJugador[i][0]=0;
-         			cordProyectilesJugador[i][1]=0;
+         			balaJugadorX=0;
+         			balaJugadorY=0;
          		}
          		
-         	
-         	
-         }
+         		//comprobamos si la bala del jugador choco contra la del enemigo
+         		colicion();
 
 	}
 	public static void crearJugador() {
-		//lateral izqueirdo
-		cordJugador[0][0]=200;
-		cordJugador[0][1]=170;
-		cordJugador[0][2]=7;
-		cordJugador[0][3]=30;
+		
 		//centro
-		cordJugador[1][0]=200;
-		cordJugador[1][1]=180;
-		cordJugador[1][2]=35;
-		cordJugador[1][3]=10;
-		//lateral derecho
-		cordJugador[2][0]=235;
-		cordJugador[2][1]=170;
-		cordJugador[2][2]=7;
-		cordJugador[2][3]=30;
+		cordJugador[0][0]=200;
+		cordJugador[0][1]=380;
+		cordJugador[0][2]=40;
+		cordJugador[0][3]=10;
+		
 		//cañon
-		cordJugador[3][0]=216;
-		cordJugador[3][1]=160;
-		cordJugador[3][2]=10;
-		cordJugador[3][3]=20;
+		cordJugador[1][0]=215;
+		cordJugador[1][1]=370;
+		cordJugador[1][2]=10;
+		cordJugador[1][3]=20;
 	
 	}
-	/*
+	public void moverBoss() {
+		
+		if(enemigos[0].x<-600) {
+			enemigos[0].x=limiteX-50;
+		}else {
+			enemigos[0].x-=30;
+		}
+		
+	}
 	public void moverEnemigos() {
-		for(int i=0;i<enemigos.length;i++) {
+		for(int i=1;i<enemigos.length;i++) {
 			switch(enemigos[i].direccion) {
 			//derecha
 			case 0:
-				//para que no se salga de la zona de juego
-				if(enemigos[i].x+30<limiteX) {
-					enemigos[i].x+=30;
-
+				
+				//resto de naves
+				if(enemigos[i].y+20<limiteY) {
+					enemigos[i].y+=20;
+					
 				}
+				
 			break;
 			//izquierda
 			case 1:
@@ -214,23 +262,13 @@ public class JuegoSpace {
 				}
 			break;
 			
-			case 2:
-				
-			break;
-			case 3:
-				
-			break;
+		
 			
 			}
 		}
 	}
-	public void cambiarDireccionEnemiga() {
-		for(int i=0;i<enemigos.length;i++) {
-			enemigos[i].direccion=rand.nextInt(2);
-		}
-		
-	}
-	*/
+	
+	
 	
 	public void pintarMapa() {
 		/*LOS PRIMEROS 2 VALORES SON LAS CORDENADAS. EL VALOR 3= LARGO DE LA BARRA. VALOR 4= ALTO DE LA BARRA
@@ -241,9 +279,8 @@ public class JuegoSpace {
 		 * 
 		 */
 	
-		
 		//barra horizontal arriba (la barra de hasta arriba)
-		cordBordesMapa[0]= new Mapa(-50,0,limiteX,10);
+		cordBordesMapa[0]= new Mapa(0,0,limiteX,10);
 		
 		//barra horizontal de abajo (la barra de hasta abajo)
 		cordBordesMapa[1]= new Mapa(0,limiteY,limiteX,10);
@@ -255,17 +292,61 @@ public class JuegoSpace {
 		cordBordesMapa[3]= new Mapa(0,0,10,limiteY);
 		
 	}
+	public static void  genProyectilEnemigo() {
+		
+		//si te genera un numerode un enemigo que ya esta destruido este no d
+		int enemigoQueDisparo = rand.nextInt(67+1);
+		
+			if(balaEnemigaX==0) {
+				balaEnemigaX= enemigos[enemigoQueDisparo].x;
+				balaEnemigaY= enemigos[enemigoQueDisparo].y;
+
+				
+				
+			}
+		
+		
+		
+	}
 	
+	public static void moverProyectilEnemigo() {
+		
+		
+			if(balaEnemigaX!=0) {
+				if(balaEnemigaY+20<limiteY) {
+					balaEnemigaY+=20;
+
+				}else {
+					balaEnemigaX=0;
+					balaEnemigaY=0;
+
+				}
+				
+				//comrobar si la bala enemiga choco con el jugador
+				if(balaEnemigaX<=cordJugador[0][0]+40 && balaEnemigaX>cordJugador[0][0]
+						&& balaEnemigaY>=cordJugador[0][1] && balaEnemigaY<=cordJugador[0][1]+10 ) {
+					
+					//aqui vas a poner un contador el cual aumentara
+					System.out.println("impacto bala enemiga con jugador linea: 330");
+					balaEnemigaX=0;
+					balaEnemigaY=0;
+
+				}
+			}
+			
+		
+	
+	}
 	public void ponerEnemigo() {
 		/*
 		 *   
 		  Aqui abajo le doy parametros del enemigo osea los datos que tendra. Cada dato se refiere a esto....
 		 *  
-		 *  posicion 0 = cordenada X  de la PRIMERA PARTE DEL CUERPO
-		 *  posicion 1= cordenada Y   de la PRIMERA PARTE DEL CUERPO
-		 *  posicion 2= largo de la parte 1 del cuerpo 
-		 *  posicion 3 = alto de la parte 1 del cuerpo
-		 *  posicion 4= color de la parte 1 del cuerpo
+		 *  posicion 0 = cordenada X  
+		 *  posicion 1= cordenada Y   
+		 *  posicion 2= largo
+		 *  posicion 3 = alto 
+		 *  posicion 4= color
 		 *  posicion 5= direccion hacia donde va el enemigo
 						
 	
@@ -319,13 +400,17 @@ public class JuegoSpace {
 		crearJugador();
 		ponerEnemigo();
 		//tiempos
-		timer.schedule(moverProyectilJugador, 0, 10);
-		timer.schedule(moverEnemigos, 0, 100);
-		timer.schedule(cambiarDireccionEnemiga, 0, 400);
+		timer.schedule(moverProyectilJugador, 0, 30);
+		timer.schedule(moverEnemigos, 0, 2000);
+		timer.schedule(genProyectilEnemigo, 0, 20);
+		timer.schedule(moverProyectilEnemigo, 0, 100);
+		timer.schedule(moverBoss, 0, 100);
+
 		
 		
 		frame.setBounds(100, 100, 834, 492);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
 		frame.getContentPane().setLayout(null);
 		
 		JPanel panel = new JPanel();
@@ -334,8 +419,14 @@ public class JuegoSpace {
 		frame.getContentPane().add(panel);
 		tablero.setBounds(0, 10, 818, 443);
 		
+		
+		
+		
+		
 		tablero.add(new MyGraphics());
 		tablero.setBackground(new Color(0, 0, 0));
+
+		frame.add(tablero);
 		frame.getContentPane().add(tablero);
 		tablero.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
@@ -351,55 +442,23 @@ public class JuegoSpace {
 
 			@Override
 			public void keyPressed(KeyEvent e) {
-
-			}
-
-			@Override
-			public void keyReleased(KeyEvent e) {
-				// TODO Auto-generated method stub
 				if(gameOver!=true) {
-					// TODO Auto-generated method stub
-					//arriba
-					if(e.getKeyCode()==87) {
-						
-						if(cordJugador[1][1]-speedJugador>20) {
-							for(int i=0;i<4;i++) {
-								cordJugador[i][1]-=speedJugador;
-
-							}
-							
-						}
-						
-		
-					}
-					//abajo
-					if(e.getKeyCode()==83) {
-						
-							if(cordJugador[1][1]+speedJugador<limiteY) {
-								for(int i=0;i<4;i++) {
-									cordJugador[i][1]+=speedJugador;
-								}
-								System.out.println("dsda");
-	
-							}	
-						
-					}
+					// TODO Auto-generated method stu
 					//izquierda
 					if(e.getKeyCode()==65) {
-						if(cordJugador[1][0]-speedJugador>0) {
-							for(int i=0;i<4;i++) {
+						if(cordJugador[0][0]-speedJugador>0+10) {
+							for(int i=0;i<2;i++) {
 								cordJugador[i][0]-=speedJugador;
 
 							}
 							
 						}
 						
-						
 					}
 					//derecha
 					if(e.getKeyCode()==68) {
-						if(cordJugador[1][0]+speedJugador<limiteX-40) {
-							for(int i=0;i<4;i++) {
+						if(cordJugador[0][0]+speedJugador<limiteX-10) {
+							for(int i=0;i<2;i++) {
 								cordJugador[i][0]+=speedJugador;
 							}
 	
@@ -422,9 +481,16 @@ public class JuegoSpace {
 						
 						
 					}
-				System.out.println(e.getKeyCode());
+					//System.out.println(cordJugador[0][0]+":    "+cordJugador[0][1]);
 				}
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				// TODO Auto-generated method stub
+				
 			}});
+		
 		
 		
 	}
@@ -441,6 +507,8 @@ public class JuegoSpace {
         public void paintComponent(Graphics g) {
             super.paintComponent(g);
 
+            
+            
             //pintar mapa
             for(int i=0;i<cordBordesMapa.length;i++) {
             	g.setColor(Color.blue);
@@ -449,58 +517,76 @@ public class JuegoSpace {
             	
             }
             //pintar balas jugador
-           
-            for(int i=0;i<cordProyectilesJugador.length;i++) {
-            
-            	if(cordProyectilesJugador[i][0]!=0) {
+        
+            if(balaJugadorX!=0) {
             		 g.setColor(Color.cyan);
-                     g.fillRect(cordProyectilesJugador[i][0],cordProyectilesJugador[i][1], 10, 20);
-            	}
-            	
+                     g.fillRect(balaJugadorX,balaJugadorY, 5, 10);
+                     
             }
+            	
+            
             //pintar jugador
-            for(int i=0;i<4;i++) {
-        	    g.setColor(Color.white);
+            for(int i=0;i<2;i++) {
+        	    g.setColor(Color.green);
                 g.fillRect(cordJugador[i][0],cordJugador[i][1], cordJugador[i][2], cordJugador[i][3]);
             }
             
             //pintarEnemigo
             //boss
-            g.drawImage(boss,enemigos[0].x,enemigos[0].y,this);
+        	if(enemigos[0].x!=0) {
+                g.drawImage(boss,enemigos[0].x,enemigos[0].y,this);
+    		}
           //pulpito
             	for(int i=1; i<16; i++) {
             		
-            		g.drawImage(enemigo3, enemigos[i].x, enemigos[i].y, this);
+            		if(enemigos[i].x!=0) {
+            			g.drawImage(enemigo3, enemigos[i].x, enemigos[i].y, this);
+            		}
+            		
             		
             	}
                 
             	for(int i=16; i<27; i++) {
-            		
-            		g.drawImage(enemigo2, enemigos[i].x, enemigos[i].y, this);
+            		if(enemigos[i].x!=0) {
+                		g.drawImage(enemigo2, enemigos[i].x, enemigos[i].y, this);
+            		}
             		
             	}
             	
             	for(int i=17; i<38; i++) {
             		
-            		g.drawImage(enemigo2, enemigos[i].x, enemigos[i].y, this);
+            		if(enemigos[i].x!=0) {
+                		g.drawImage(enemigo2, enemigos[i].x, enemigos[i].y, this);
+            		}
+            		
             		
             	}
             	
-            		for(int i=38; i<53; i++) {
-            		
-            		g.drawImage(enemigo1, enemigos[i].x, enemigos[i].y, this);
+            	for(int i=38; i<53; i++) {
+            			if(enemigos[i].x!=0) {
+                    		g.drawImage(enemigo1, enemigos[i].x, enemigos[i].y, this);	
+                		}
+                		
             		
             	}
             		
             		for(int i=53; i<68; i++) {
                 		
-                		g.drawImage(enemigo1, enemigos[i].x, enemigos[i].y, this);
-                		
+            			if(enemigos[i].x!=0) {
+                    		g.drawImage(enemigo1, enemigos[i].x, enemigos[i].y, this);
+                		}	
                 	}
             //	g.drawImage(enemigo2,100,100,this);
-            	
-            
-            
+            		
+            		
+           //pintar proyectiles enemigos
+           
+        	   if(balaEnemigaX!=0) {
+                  	g.setColor(Color.red);
+                      g.fillRect(balaEnemigaX,balaEnemigaY,5,10);
+
+              	}
+           
            
            tablero.repaint();
       
