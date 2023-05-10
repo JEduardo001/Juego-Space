690ef251a7c5aacb196d9cc63a491102d51c4b91
JoaquinVargas1 <127267386+JoaquinVargas1@users.noreply.github.com>
2023-05-09 23:57:43 -0700

    Add files via upload

---
+++ Juego-Space/Juego-Space/Space/src/Space/Mapa.java
@@ -0,0 +1,12 @@
+package Space;
+
+public class Mapa {
+
+	int x,y,largo,alto;
+	public Mapa(int x,int y,int largo,int alto) {
+		this.x=x;
+		this.y=y;
+		this.largo=largo;
+		this.alto=alto;
+	}
+}
